9920be65073440a9cf1845f9b9b2e911b312c263
nicknickovich <nick.poberezhnyk@gmail.com>
2020-05-05 12:56:27 +0300

    Minimal working application

---
+++ backend/app/__init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+
+
+def create_app(config_file):
+    app = Flask(__name__, instance_relative_config=True)
+    with app.app_context():
+        app.config.from_pyfile("config.py")
+        environment = os.getenv("FLASK_ENV")
+        if environment == "production":
+            app.config.from_pyfile("prod.py")
+        elif environment == "development":
+            app.config.from_pyfile("dev.py")
+        elif environment == "testing":
+            app.config.from_pyfile("test.py")
+
+        from app.controllers import main
+        app.register_blueprint(main)
+
+    return app

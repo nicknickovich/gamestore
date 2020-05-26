1b5bbf734147eabbc963182f897ce418d1e45d3f
nicknickovich <nick.poberezhnyk@gmail.com>
2020-05-26 19:08:18 +0300

    User api routes

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, request
 from flask_restful import Resource
-from app import api_restful, db
+from app import api_restful, db, schemas
 from app.models import User
-from app import schemas
 
 
 api = Blueprint("api", __name__)
@@ -23,8 +22,22 @@ class UserRoutes(Resource):
         db.session.commit()
         return user_schema.dump(new_user), 201
 
+    def put(self, id):
+        user = User.query.get_or_404(id)
+        updated_user = user_schema.load(
+            request.json, instance=user, session=db.session
+        )
+        db.session.commit()
+        return user_schema.dump(updated_user)
+
+    def delete(self, id):
+        user = User.query.get_or_404(id)
+        db.session.delete(user)
+        db.session.commit()
+        return "", 204
+
 api_restful.add_resource(
     UserRoutes,
     "/api/users",
-    "/api/users/<int:id>"
+    "/api/users/<id>"
 )
